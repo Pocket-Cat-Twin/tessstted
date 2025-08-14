aeebad37c26b69ff048fe62a4b835713d7c36eaf
grakkeroyewlahadanz <githuncodespace@proton.me>
2025-08-14 11:58:43 +0000

    412341234

--- apps/web/src/lib/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all UI components
-export { default as Button } from './Button.svelte';
-export { default as Badge } from './Badge.svelte';
-export { default as Card } from './Card.svelte';
-export { default as Input } from './Input.svelte';
-export { default as Modal } from './Modal.svelte';
-export { default as Spinner } from './Spinner.svelte';
-export { default as Toast } from './Toast.svelte';
